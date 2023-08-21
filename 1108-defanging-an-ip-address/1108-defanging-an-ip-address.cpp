a9e0160ce10be42589f45172aa9106907d88a859
Gandhi Monil <85578666+Monil-007@users.noreply.github.com>
2023-08-21 23:10:48 +0530

    Time: 2 ms (44.16%), Space: 6 MB (60.04%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string s="";
+        for(auto i:address){
+            if(i=='.'){s+="[.]";}
+            else{s+=i;}
+        }
+        return s;
+    }
+};
